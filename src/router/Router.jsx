c2b4467c88ab39c5e0e386d86f4a5dd79a2fc3c3
Tikpeme <tete.ikpeme@gmail.com>
2022-12-11 13:46:27 -0500

    Deployment prep

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AnimatePresence } from "framer-motion";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "../pages/Home";
 import Results from "../components/Results";
 import ErrorPage from "../components/ErrorPage";
@@ -12,23 +12,25 @@ function Router() {
   const location = useLocation();
 
   return (
-    <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/searched/:searchTerm"
-          element={
-            <Results>
-              {" "}
-              <Navigation />{" "}
-            </Results>
-          }
-        />
-        <Route path="/Card/:cardId" element={<Cardpage />} />
-        <Route path="/Error/:cardName" element={<ErrorPage />} />
-        <Route path="/filter/:searchTerm" element={<Filter />} />
-      </Routes>
-    </AnimatePresence>
+    <BrowserRouter>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/searched/:searchTerm"
+            element={
+              <Results>
+                {" "}
+                <Navigation />{" "}
+              </Results>
+            }
+          />
+          <Route path="/Card/:cardId" element={<Cardpage />} />
+          <Route path="/Error/:cardName" element={<ErrorPage />} />
+          <Route path="/filter/:searchTerm" element={<Filter />} />
+        </Routes>
+      </AnimatePresence>
+    </BrowserRouter>
   );
 }
 
